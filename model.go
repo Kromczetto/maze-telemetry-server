cbb1c4119713418c21f9db3c604d98940f2653f3
Jakub <jakubkromolowski@gmail.com>
2026-04-29 20:05:23 +0100

    Add maze size and wall

--- model.go
+++ model.go
@@ -3,10 +3,16 @@ package main
 import "time"
 
 type Run struct {
-	Time int64 `json:"time" bson:"time"`
-	Cells int `json:"cells" bson:"cells"`
-	Turns int `json:"turns" bson:"turns"`
-	Algorithm string `json:"algorithm" bson:"algorithm"`
-	Maze []int `json:"maze" bson:"maze"`
+	Time      int64     `json:"time" bson:"time"`
+	Cells     int       `json:"cells" bson:"cells"`
+	Turns     int       `json:"turns" bson:"turns"`
+	Algorithm string    `json:"algorithm" bson:"algorithm"`
+
+	Width     int       `json:"width" bson:"width"`
+	Height    int       `json:"height" bson:"height"`
+
+	Maze      []int     `json:"maze" bson:"maze"` 
+	Walls     []int     `json:"walls" bson:"walls"`
+
 	CreatedAt time.Time `json:"created_at" bson:"created_at"`
 }
